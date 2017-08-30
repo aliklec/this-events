0c431f5227ab42a9a42b3ceca0e24d6e8c5bc243
Alison Kleczewski <akleczew@gmail.com>
2017-08-29 21:56:29 -1000

    updated

--- js/app.js
+++ js/app.js
@@ -2,3 +2,20 @@
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
+
+
+var hiddenMenu = document.getElementsByClassName("name");
+for(var i = 0; i<hiddenMenu.length; i++){
+  hiddenMenu[i].addEventListener("click", showHidden);
+  console.log(hiddenMenu[i]);
+}
+
+function showHidden(){
+  var showHidden = this.querySelectorAll(".menu")[0]; //this referring to "i"?
+  if(showHidden.style.display === "none"){
+    showHidden.style.display = "block";
+  }else{
+    showHidden.style.display = "none";
+  }
+}
+
